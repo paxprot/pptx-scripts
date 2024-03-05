cbd073ec62ef419788fde1682b5ab81b38ae252c
paxprot <30484122+paxprot@users.noreply.github.com>
2024-03-05 13:50:07 +0800

    autoplay audio with animated slides

--- insert_audio.py
+++ insert_audio.py
@@ -4,6 +4,18 @@ from pptx import Presentation
 from pptx.util import Inches
 from lxml import etree
 
+def autoplay_media(media):
+    el_id = xpath(media.element, './/p:cNvPr')[0].attrib['id']
+    el_cnt = xpath(
+        media.element.getparent().getparent().getparent(),
+        './/p:timing//p:video//p:spTgt[@spid="%s"]' % el_id,
+    )[0]
+    cond = xpath(el_cnt.getparent().getparent(), './/p:cond')[0]
+    cond.set('delay', '0')
+
+def xpath(el, query):
+    nsmap = {'p': 'http://schemas.openxmlformats.org/presentationml/2006/main'}
+    return etree.ElementBase.xpath(el, query, namespaces=nsmap)
 
 def insert_audio_to_pptx(pptx_file):
     # Load the presentation
@@ -29,10 +41,8 @@ def insert_audio_to_pptx(pptx_file):
             )
 
             # Get this workaround to generate autoplay audio from:
-            # https://github.com/scanny/python-pptx/issues/427#issuecomment-466217611
-            tree = audio_shape._element.getparent().getparent().getnext().getnext()
-            timing = [el for el in tree.iterdescendants() if etree.QName(el).localname == 'cond'][0]
-            timing.set('delay', '0')
+            # https://github.com/scanny/python-pptx/issues/427#issuecomment-856724440
+            autoplay_media(audio_shape)
 
     # Save the modified presentation
     output_pptx_file = os.path.splitext(pptx_file)[0] + "_with_audio.pptx"
